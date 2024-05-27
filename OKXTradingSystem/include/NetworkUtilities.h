b6958b2d433dec4037972a8334798a09dcd36468
mihikakrishna <krishna.mihika@gmail.com>
2024-05-27 08:24:49 -0700

    More cleanup

--- OKXTradingSystem/include/NetworkUtilities.h
+++ OKXTradingSystem/include/NetworkUtilities.h
@@ -4,12 +4,7 @@
 #include <curl/curl.h>
 #include <string>
 
-// Callback function for writing received data
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp);
-
 std::string cleanHeader(const std::string& header);
-
 std::string get_iso8601_timestamp();
-
-// Function to send a request and receive a response
 std::string sendRequest(const std::string& url, const std::string& postData = "", const std::string& method = "GET", const std::string& api_key = "", const std::string& secret_key = "", const std::string& passphrase = "");
